6b5c2c49d7952722ab7e9420989b3051f4de7b90
Dmytro <dmitryohanovphotography@gmail.com>
2023-01-29 19:10:32 +0000

    aws deployment & text update

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ const start = () => {
         try {
             if (text === '/start') {
                 await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-                return bot.sendMessage(chatId, `Welcome to the bot created by @ohanovdmytro`);
+                return bot.sendMessage(chatId, `Welcome to the "Guess-the-number" bot created by @ohanovdmytro!`);
             }
             if(text === '/info') {
-                return bot.sendMessage(chatId, `Your name is ${msg.from.first_name} ${msg.from.last_name}`);
+                return bot.sendMessage(chatId, `I know you! Your name is ${msg.from.first_name} ${msg.from.last_name}`);
             }
     
             if(text === '/game') {
@@ -60,7 +60,7 @@ const start = () => {
             return startGame(chatId)
         }
         if (data == chats[chatId]) {
-            await bot.sendMessage(chatId, `Great! I have ordered number ${chats[chatId]}!`, againOptions);
+            await bot.sendMessage(chatId, `Great! I have ordered number ${chats[chatId]} :=D`, againOptions);
         } else {
             await bot.sendMessage(chatId, `Sorry, but my number is ${chats[chatId]} =(`, againOptions);
         }
@@ -69,3 +69,12 @@ const start = () => {
 }
 
 start();
+
+exports.handler = (event, context, callback) => {
+    const tmp = JSON.parse(event.body);
+    bot.handleUpdate(tmp);
+    return callback (null, {
+        statusCode: 200,
+        body: ''
+    });
+}
